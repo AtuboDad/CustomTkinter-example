4f4408df09a84c6c2a8a6846ff8e1e7007289354
Chuanjian Lai <lcjasas@sina.com>
2024-06-19 16:56:07 +0800

    place window in center

--- custom/login_example.py
+++ custom/login_example.py
@@ -12,9 +12,16 @@ class App(customtkinter.CTk):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        # 获取屏幕宽高
+        window_width = self.winfo_screenwidth()
+        window_height = self.winfo_screenheight()
+
+        x = (window_width - self.winfo_width()) // 2
+        y = (window_height - self.winfo_height()) // 2
+
         # configure window
         self.title("XXX soft")
-        self.geometry(f"{700}x{450}")
+        self.geometry(f"{self.width}x{self.height}+{x}+{y}")
 
         # create login frame
         self.login_frame = LoginFrame(master=self)
